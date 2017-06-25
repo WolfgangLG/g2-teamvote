9146af51c6e825eadce8e18267efb950ac5f0c80
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-06-25 11:20:29 -0500

    Persist employees to the database

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,6 +2,16 @@ class Employee < ActiveRecord::Base
 
   def self.find_employees
     response = HTTParty.get("https://api.myjson.com/bins/16roa3")
-    json = JSON.parse(response.body)
+    team_members = JSON.parse(response.body)
+
+    team_members.map do |atr|
+      team_member = Employee.find_or_create_by(g2employee_id:atr['id']) do |employee|
+        employee.name = atr['name']
+        employee.image_url = atr['image_url']
+        employee.title = atr['title']
+        employee.bio = atr['bio']
+      end
+      team_member
+    end
   end
 end
